02201451b0d489b6359685a6997a5b49f67352f6
Ammar Lakis <ammar.lakis@gmail.com>
2014-07-25 21:16:44 +0300

    populate changes horizonally and vertically

--- js/GameManager.js
+++ js/GameManager.js
@@ -1,5 +1,8 @@
 var activeColor = 'yellow';
 var nactiveColor= 'grey';
+var modes = [];
+modes[0] = 'nactive';
+modes[1] = 'active';
 function GameManager(){
     this.level = [
         [
@@ -11,38 +14,51 @@ function GameManager(){
     ];
     this.generateGame = function (){
         var game = '';
-        var modes = [];
-        modes[0] = 'nactive';
-        modes[1] = 'active';
         for(var row = 0; row < this.level.length; row++){
             for(var col = 0; col < this.level[row].length; col++){
-                game += '<div onclick="gameManager.flip(this);" id="'+row+'-'+col+'" class="box small '+modes[this.level[row][col]]+'" style="-webkit-transition: 0ms 2000ms; transition: 0ms 2000ms;"></div>' + "\n";
+                game += '<div onclick="gameManager.flip(this);" id="t'+row+'-'+col+'" class="box small '+modes[this.level[row][col]]+'" style="-webkit-transition: 0ms 2000ms; transition: 0ms 2000ms;"></div>' + "\n";
             }
-            game += '<br />' + "\n";
+            game += '<br />' + '<br />' + '<br />' + "\n";
         }
-        $(game).appendTo('body');
+        $(game).appendTo('div.container');
     }
     this.flip = function flip(e){
         console.log(e);
-        move(e)
-        .set('background-color',e.classList.contains('active')? nactiveColor:activeColor)
-        .delay('0.1s')
-        .end();
-        if (e.classList.contains('active')){
-            e.classList.remove('active'); e.classList.add('nactive');
-        } else {
-            e.classList.remove('nactive'); e.classList.add('active');
-        }
-        var match = e.getAttribute('id').match(/\d+/g);
-        var nrow = parseInt(match[0], 10);
-        var ncol = parseInt(match[1], 10);
-        this.level[nrow][ncol] = 1 - this.level[nrow][ncol];
+        flipColor(e);
+        flipTiles(e);
 
         if (this.checkIfWin())
             alert('you won !');
-        function getTarget(e){
-            e=e||window.event;
-            return (e.target||e.srcElement);
+        function flipColor(e){
+            move(e)
+            .set('background-color',e.classList.contains('active')? nactiveColor:activeColor)
+            .delay('0.1s')
+            .end();
+            if (e.classList.contains('active')){
+                e.classList.remove('active'); e.classList.add('nactive');
+            } else {
+                e.classList.remove('nactive'); e.classList.add('active');
+            }
+        }
+        function flipTiles(e){
+            var match = e.getAttribute('id').match(/\d+/g);
+            var nrow = parseInt(match[0], 10);
+            var ncol = parseInt(match[1], 10);
+            var level = gameManager.level;
+            console.log(level[nrow][ncol]);
+            level[nrow][ncol] = 1 - level[nrow][ncol];
+            for(var row = nrow; (row < level.length) && (level[row][ncol] != 2); row++){
+                level[row][ncol] = 1 - level[row][ncol];
+            }
+            for(var row = nrow; (row >= 0) && (level[row][ncol] != 2); row--){
+                level[row][ncol] = 1 - level[row][ncol];
+            }
+            for(var col = ncol; (col < level[nrow].length) && (level[nrow][col] != 2); col++){
+                level[nrow][col] = 1 - level[nrow][col];
+            }
+            for(var col = ncol; (col >= 0) && (level[nrow][col] != 2); col--){
+                level[nrow][col] = 1 - level[nrow][col];
+            }
         }
     }
     this.checkIfWin = function checkIfWin(){
