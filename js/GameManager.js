0f4b45328ee51399ae0e9b3c46ec601963047fc1
Ammar Lakis <ammar.lakis@gmail.com>
2014-07-31 13:38:51 +0300

    add moves penality

--- js/GameManager.js
+++ js/GameManager.js
@@ -2,28 +2,42 @@ var activeColor = 'rgba(255, 255, 0, 0.7)';
 var nactiveColor= 'rgba(50, 50, 0, 0.2)';
 var modes = ['nactive','active','disabled'];
 function GameManager(){
-    // Read level
     this.score = 0;
+    this.moves = 0;
+    // Read level
     this.levelRank = 1;
     this.level = new Level(this.levelRank);
     // Resize container where 82 is one cell width and height
     //generate game in html
     this.generateGame = function (){
+        //update with score
+        console.log(gameManager.score);
+        if (gameManager.score == 0){
+            gameManager.updateScore(0);
+        }
+        //reset moves counter for every game
+        this.moves = 0;
+        //initialize game grid
+        var grid = this.level.grid;
+        //hide gameoverBoard
         $('#gameoverBoard').css({
             opacity: 0,
             visibility: 'hidden',
             '-webkit-transition': '',
             transition: ''
         });
+        //disable gameoverBoard
         $('#gameoverBoard').attr('onclick','');
-        $('#gameGrid').css('height',this.level.length * 82);
-        $('#gameGrid').css('width',this.level[0].length * 82);
+        //resize gamegrid based on level grid size
+        $('#gameGrid').css('height',grid.length * 82);
+        $('#gameGrid').css('width',grid[0].length * 82);
+        //reset game html content
         var game = '';
         // loop on every level matrix element and add html tile element with coressponding id and flip function
-        for(var row = 0; row < this.level.length; row++){
-            for(var col = 0; col < this.level[row].length; col++){
-                var flipFunc = this.level[row][col] == 2 ? '' : 'gameManager.flip(this);'
-                game += '<div onclick="' + flipFunc + '" id="t'+row+'-'+col+'" class="box small '+modes[this.level[row][col]]+'" style="-webkit-transition: 0ms 2000ms; transition: 0ms 2000ms;"></div>' + "\n";
+        for(var row = 0; row < grid.length; row++){
+            for(var col = 0; col < grid[row].length; col++){
+                var flipFunc = grid[row][col] == 2 ? '' : 'gameManager.flip(this);'
+                game += '<div onclick="' + flipFunc + '" id="t'+row+'-'+col+'" class="box small '+modes[grid[row][col]]+'" style="-webkit-transition: 0ms 2000ms; transition: 0ms 2000ms;"></div>' + "\n";
             }
             game += "\n";
         }
@@ -33,7 +47,27 @@ function GameManager(){
     }
     // flip tile after pressed
     this.flip = function (e){
-        //console.log(e);
+        //check if moves have exceed
+        if (this.moves >= this.level.maxMoves){
+            //reset the game
+            gameManager.levelRank = 0;
+            gameManager.score = 0;
+            //disable all the boxes
+            $('.box').attr('onclick','');
+            //enable and display gameoverBoard
+            $('#gameoverBoard').css({
+                opacity: 100,
+                visibility: 'visible',
+                '-webkit-transition': 'opacity 2s ease-in-out',
+                transition: 'opacity 2s ease-in-out'
+            });
+            $('#gameoverBoard').html('Gameover !');
+            //move to the next level
+            $('#gameoverBoard').attr('onclick','gameManager.nextLevel()');
+            return;
+        }
+        //add 1 to moves counter
+        this.moves ++;
         //flip that tile color
         flipColor(e);
         //and corresponding vertical and horizonal tiles
@@ -42,15 +76,20 @@ function GameManager(){
         setTimeout(function(){
             //after changing color, check if game is over
             if (gameManager.checkIfWin()){
+                //disable all the boxes
                 $('.box').attr('onclick','');
+                //enable and display gameoverBoard
                 $('#gameoverBoard').css({
                     opacity: 100,
                     visibility: 'visible',
                     '-webkit-transition': 'opacity 2s ease-in-out',
                     transition: 'opacity 2s ease-in-out'
                 });
+                //move to the next level
                 $('#gameoverBoard').attr('onclick','gameManager.nextLevel()');
-                gameManager.updateScore(gameManager.levelRank);
+                $('#gameoverBoard').html('Next Level >>');
+                //add score
+                gameManager.updateScore(gameManager.level.maxMoves - gameManager.moves);
             }
         },350);
         function flipColor(e){
@@ -73,42 +112,42 @@ function GameManager(){
             var nrow = parseInt(match[0], 10);
             var ncol = parseInt(match[1], 10);
             
-            var level = gameManager.level;
+            var grid = gameManager.level.grid;
             //console.log(level[nrow][ncol]);
             // flip the tile
-            level[nrow][ncol] = 1 - level[nrow][ncol];
+            grid[nrow][ncol] = 1 - grid[nrow][ncol];
             //iterate and flip down the tiles
-            for(var row = nrow + 1; (row < level.length) && (level[row][ncol] != 2); row++){
+            for(var row = nrow + 1; (row < grid.length) && (grid[row][ncol] != 2); row++){
                 console.log('flipping down');
                 flipColor($('#'+'t'+row+'-'+ncol)[0]);
-                level[row][ncol] = 1 - level[row][ncol];
+                grid[row][ncol] = 1 - grid[row][ncol];
             }
             //iterate and flip up the tiles
-            for(var row = nrow - 1; (row >= 0) && (level[row][ncol] != 2); row--){
+            for(var row = nrow - 1; (row >= 0) && (grid[row][ncol] != 2); row--){
                 console.log('flipping up');
                 flipColor($('#'+'t'+row+'-'+ncol)[0]);
-                level[row][ncol] = 1 - level[row][ncol];
+                grid[row][ncol] = 1 - grid[row][ncol];
             }
             //iterate and flip right the tiles
-            for(var col = ncol + 1; (col < level[nrow].length) && (level[nrow][col] != 2); col++){
+            for(var col = ncol + 1; (col < grid[nrow].length) && (grid[nrow][col] != 2); col++){
                 console.log('flipping right');
                 flipColor($('#'+'t'+nrow+'-'+col)[0]);
-                level[nrow][col] = 1 - level[nrow][col];
+                grid[nrow][col] = 1 - grid[nrow][col];
             }
             //iterate and flip left the tiles
-            for(var col = ncol - 1; (col >= 0) && (level[nrow][col] != 2); col--){
+            for(var col = ncol - 1; (col >= 0) && (grid[nrow][col] != 2); col--){
                 console.log('flipping left');
                 flipColor($('#'+'t'+nrow+'-'+col)[0]);
-                level[nrow][col] = 1 - level[nrow][col];
+                grid[nrow][col] = 1 - grid[nrow][col];
             }
         }
     }
     // check if game is over
     this.checkIfWin = function (){
         //iterate on level matrix and check if any tile is not activated
-        for(var row = 0; row < this.level.length; row++){
-            for(var col = 0; col < this.level[row].length; col++){
-                if(this.level[row][col] == 0)
+        for(var row = 0; row < this.level.grid.length; row++){
+            for(var col = 0; col < this.level.grid[row].length; col++){
+                if(this.level.grid[row][col] == 0)
                     return false
             }
         }
--- js/Level.js
+++ js/Level.js
@@ -1,62 +1,65 @@
 function Level(rank){
     this.level = [];
-        this.level[0] = [
+        this.level[0] = {
+            maxMoves:10,
+            grid: [
              [
                  0,0
              ]
-         ];
-         this.level[1] = [
+         ]};
+         this.level[1] = {
+            maxMoves:10,
+            grid: [
              [
                  0,0
              ],
              [
                  0,1
              ]
-         ];            
-         this.level[2] = [
+         ]};            
+         this.level[2] = {
+            maxMoves:10,
+            grid: [
              [
                  0,1
              ],
              [
                  0,2
              ]
-         ];            
-         this.level[3] = [
+         ]};            
+         this.level[3] = {
+            maxMoves:10,
+            grid: [
              [
                  0,1,1
              ],
              [
                  0,2,0
              ]
-         ];
-         this.level[4] = [
+         ]};
+         this.level[4] = {
+            maxMoves:10,
+            grid: [
              [
                  0,0,0
              ],
              [
                  0,0,0
              ]
-         ];
-         this.level[5] = [
+         ]};
+         this.level[5] = {
+            maxMoves:10,
+            grid: [
              [
                  1,1,0,1
              ],
              [
                  0,0,0,0
              ]
-         ];
-         this.level[6] = [
-             [
-                 1,1,0,1
-             ],
-             [
-                 0,0,0,0
-             ],
-             [
-                 1,1,0,1
-             ]
-         ];
-         this.level[7] = [
+         ]};
+         this.level[6] = {
+            maxMoves:10,
+            grid: [
              [
                  1,1,0,1
              ],
@@ -66,8 +69,10 @@ function Level(rank){
              [
                  1,1,0,1
              ]
-         ];
-         this.level[8] = [
+         ]};
+         this.level[7] = {
+            maxMoves:10,
+            grid: [
              [
                  1,1,0,1
              ],
@@ -77,8 +82,10 @@ function Level(rank){
              [
                  1,1,0,1
              ]
-         ];
-         this.level[9] = [
+         ]};
+         this.level[8] = {
+            maxMoves:10,
+            grid: [
              [
                  1,1,0,1
              ],
@@ -88,8 +95,10 @@ function Level(rank){
              [
                  1,1,0,1
              ]
-         ];
-         this.level[10] = [
+         ]};
+         this.level[9] = {
+            maxMoves:10,
+            grid: [
              [
                  1,1,0,1
              ],
@@ -99,8 +108,10 @@ function Level(rank){
              [
                  1,1,0,1
              ]
-         ];
-         this.level[11] = [
+         ]};
+         this.level[10] = {
+            maxMoves:10,
+            grid: [
              [
                  1,1,0,1
              ],
@@ -110,7 +121,20 @@ function Level(rank){
              [
                  1,1,0,1
              ]
-         ];
+         ]};
+         this.level[11] = {
+            maxMoves:10,
+            grid: [
+             [
+                 1,1,0,1
+             ],
+             [
+                 0,0,0,0
+             ],
+             [
+                 1,1,0,1
+             ]
+         ]};
     if(rank > this.level.length)
         return this.level[this.level.length - 1];
     return this.level[rank-1];
